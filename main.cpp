2665b3934150c4e1fbfc44a994724ead7773929d
Lars Pensjo <lars.pensjo@gmail.com>
2013-08-06 21:43:05 +0200

    Re-indent using TAB instead of 2-space.

--- main.cpp
+++ main.cpp
@@ -3,47 +3,47 @@
 
 static void helloWorld (GtkWidget *wid, GtkWidget *win)
 {
-  GtkWidget *dialog = NULL;
+	GtkWidget *dialog = NULL;
 
-  dialog = gtk_message_dialog_new (GTK_WINDOW (win), GTK_DIALOG_MODAL, GTK_MESSAGE_INFO, GTK_BUTTONS_CLOSE, "Hello World!");
-  gtk_window_set_position (GTK_WINDOW (dialog), GTK_WIN_POS_CENTER);
-  gtk_dialog_run (GTK_DIALOG (dialog));
-  gtk_widget_destroy (dialog);
+	dialog = gtk_message_dialog_new (GTK_WINDOW (win), GTK_DIALOG_MODAL, GTK_MESSAGE_INFO, GTK_BUTTONS_CLOSE, "Hello World!");
+	gtk_window_set_position (GTK_WINDOW (dialog), GTK_WIN_POS_CENTER);
+	gtk_dialog_run (GTK_DIALOG (dialog));
+	gtk_widget_destroy (dialog);
 }
 
 int main (int argc, char *argv[])
 {
-  GtkWidget *button = NULL;
-  GtkWidget *win = NULL;
-  GtkWidget *vbox = NULL;
+	GtkWidget *button = NULL;
+	GtkWidget *win = NULL;
+	GtkWidget *vbox = NULL;
 
-  /* Initialize GTK+ */
-  g_log_set_handler ("Gtk", G_LOG_LEVEL_WARNING, (GLogFunc) gtk_false, NULL);
-  gtk_init (&argc, &argv);
-  g_log_set_handler ("Gtk", G_LOG_LEVEL_WARNING, g_log_default_handler, NULL);
+	/* Initialize GTK+ */
+	g_log_set_handler ("Gtk", G_LOG_LEVEL_WARNING, (GLogFunc) gtk_false, NULL);
+	gtk_init (&argc, &argv);
+	g_log_set_handler ("Gtk", G_LOG_LEVEL_WARNING, g_log_default_handler, NULL);
 
-  /* Create the main window */
-  win = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-  gtk_container_set_border_width (GTK_CONTAINER (win), 8);
-  gtk_window_set_title (GTK_WINDOW (win), "Hello World");
-  gtk_window_set_position (GTK_WINDOW (win), GTK_WIN_POS_CENTER);
-  gtk_widget_realize (win);
-  g_signal_connect (win, "destroy", gtk_main_quit, NULL);
+	/* Create the main window */
+	win = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+	gtk_container_set_border_width (GTK_CONTAINER (win), 8);
+	gtk_window_set_title (GTK_WINDOW (win), "Hello World");
+	gtk_window_set_position (GTK_WINDOW (win), GTK_WIN_POS_CENTER);
+	gtk_widget_realize (win);
+	g_signal_connect (win, "destroy", gtk_main_quit, NULL);
 
-  /* Create a vertical box with buttons */
-  vbox = gtk_grid_new ();
-  gtk_container_add (GTK_CONTAINER (win), vbox);
+	/* Create a vertical box with buttons */
+	vbox = gtk_grid_new ();
+	gtk_container_add (GTK_CONTAINER (win), vbox);
 
-  button = gtk_button_new_from_stock (GTK_STOCK_DIALOG_INFO);
-  g_signal_connect (G_OBJECT (button), "clicked", G_CALLBACK (helloWorld), (gpointer) win);
-  gtk_grid_attach(GTK_GRID(vbox), button, 0, 0, 1, 1);
+	button = gtk_button_new_from_stock (GTK_STOCK_DIALOG_INFO);
+	g_signal_connect (G_OBJECT (button), "clicked", G_CALLBACK (helloWorld), (gpointer) win);
+	gtk_grid_attach(GTK_GRID(vbox), button, 0, 0, 1, 1);
 
-  button = gtk_button_new_from_stock (GTK_STOCK_CLOSE);
-  g_signal_connect (button, "clicked", gtk_main_quit, NULL);
-  gtk_grid_attach(GTK_GRID(vbox), button, 0, 1, 1, 1);
+	button = gtk_button_new_from_stock (GTK_STOCK_CLOSE);
+	g_signal_connect (button, "clicked", gtk_main_quit, NULL);
+	gtk_grid_attach(GTK_GRID(vbox), button, 0, 1, 1, 1);
 
-  /* Enter the main loop */
-  gtk_widget_show_all (win);
-  gtk_main ();
-  return 0;
+	/* Enter the main loop */
+	gtk_widget_show_all (win);
+	gtk_main ();
+	return 0;
 }
