f55fe1e131bef6d65da8945e2b3537124c433424
Lars Pensjo <lars.pensjo@gmail.com>
2013-08-10 15:59:53 +0200

    Detect back utf-8.

--- main.cpp
+++ main.cpp
@@ -15,9 +15,11 @@ static void helloWorld (GtkWidget *wid, GtkWidget *win)
 
 void LoadFile(const char *fileName, GtkTextBuffer *dest) {
 	using std::ios;
+	using std::cout;
+	using std::endl;
 	std::ifstream input(fileName);
 	if (!input.is_open()) {
-		std::cerr << "Failed to open " << fileName << std::endl;
+		std::cerr << "Failed to open " << fileName << endl;
 		exit(1);
 	}
 	auto begin = input.tellg();
@@ -25,12 +27,16 @@ void LoadFile(const char *fileName, GtkTextBuffer *dest) {
 	auto end = input.tellg();
 	input.seekg (0, ios::beg);
 	auto size = end - begin;
-	if (size > 10000)
-		size = 10000;
 	char *buff = new char[size+1];
 	input.read(buff, size);
+	const char *last;
+	bool ok = g_utf8_validate(buff, size, &last);
+	if (!ok) {
+		cout << "Bad character at pos " << last - buff << endl;
+		size = last - buff;
+	}
 	buff[size] = 0;
-	std::cout << "Read " << size << " characters from " << fileName << std::endl;
+	std::cout << "Read " << size << " characters from " << fileName << endl;
 	gtk_text_buffer_set_text(dest, buff, -1);
 	delete [] buff;
 }
